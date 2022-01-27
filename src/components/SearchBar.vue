d850d4d8eb4a6b0c61e77a919dee389afdcbf67f
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-01-27 22:56:30 +0300

    added fetching matching book titles

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,15 +3,23 @@
     <input type="text" v-model="query" />
     <ul>
       <li v-if="autocompleteIsActive">loading...</li>
-      <li v-for="book in matchingBooks" :key="book.key">{{ book.title }}</li>
+      <div v-if="!autocompleteIsActive">
+        <li
+          class="autocomplete-result"
+          v-for="book in matchingBooks"
+          :key="book.key"
+          @click="onItemChange"
+        >
+          <span class="title">{{ book.title }}</span> by
+          <span class="author">{{ book.author }}</span>
+        </li>
+      </div>
     </ul>
     <button type="submit">Submit</button>
   </form>
 </template>
 
 <script>
-// import store from "../store/index.js";
-
 export default {
   data() {
     return {
@@ -23,27 +31,35 @@ export default {
   methods: {
     onBookSearchSubmit(e) {
       e.preventDefault();
-      console.log(this.query);
     },
     async searchBook() {
       const res = await fetch(
-        "http://openlibrary.org/search.json?q=" + this.query.trim()
+        "http://openlibrary.org/search.json?title=" + this.query.trim()
       );
       console.log(this.query);
       const response = await res.json();
       if (response) {
-        this.matchingBooks = [...response.docs];
+        for (const book of response.docs) {
+          this.matchingBooks.push({
+            title: book.title,
+            author: book.author_name[0],
+            key: book.key,
+          });
+        }
       } else {
         alert("Error while searching for book");
       }
       this.autocompleteIsActive = false;
     },
+    onItemChange(e) {
+      console.log(e.target.key);
+    },
   },
   watch: {
     query() {
       if (this.query.length > 3) {
-        this.autocompleteIsActive = true;
         this.matchingBooks = [];
+        this.autocompleteIsActive = true;
         this.searchBook();
       }
     },
@@ -51,4 +67,12 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.author {
+  font-weight: bold;
+  color: darkblue;
+}
+.autocomplete-result {
+  cursor: pointer;
+}
+</style>
